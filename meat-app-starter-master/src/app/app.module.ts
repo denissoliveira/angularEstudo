8a6980a8c513acc5f35395c926d31d4caa104e27
denissoliveira <denissoliveira@gmail.com>
2018-12-08 18:23:32 -0300

    feat: Criano o Componente de Login

--- meat-app-starter-master/src/app/app.module.ts
+++ meat-app-starter-master/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { OrderSummaryComponent } from './order/order-summary/order-summary.compo
 import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { LoginComponent } from './security/login/login.component';
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
     MenuItemComponent,
     ReviewsComponent,
     OrderSummaryComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- meat-app-starter-master/src/app/app.routes.ts
+++ meat-app-starter-master/src/app/app.routes.ts
@@ -1,23 +1,25 @@
-import { Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { RestaurantsComponent } from "./restaurants/restaurants.component";
-import { RestaurantDetailComponent } from "./restaurants/restaurant-detail/restaurant-detail.component";
-import { MenuComponent } from "./restaurants/restaurant-detail/menu/menu.component";
-import { ReviewsComponent } from "./restaurants/restaurant-detail/reviews/reviews.component";
-import { OrderSummaryComponent } from "./order/order-summary/order-summary.component";
-import { NotFoundComponent } from "./not-found/not-found.component";
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurants/restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurants/restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurants/restaurant-detail/reviews/reviews.component';
+import { OrderSummaryComponent } from './order/order-summary/order-summary.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './security/login/login.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', loadChildren: './about/about.module#AboutModule'}, //lazyModule
+    {path: 'login', component: LoginComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'}, // lazyModule
     {path: 'restaurants', component: RestaurantsComponent},
     {path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
-            {path: '', redirectTo: 'menu', pathMatch: 'full'}, //Após 'restaurants/:id' se não digitar nada, é redirecionado para menu
+            {path: '', redirectTo: 'menu', pathMatch: 'full'}, // Após 'restaurants/:id' se não digitar nada, é redirecionado para menu
             {path: 'menu', component: MenuComponent},
             {path: 'reviews', component: ReviewsComponent}
     ]},
     {path: 'order-summary', component: OrderSummaryComponent},
-    {path: 'order', loadChildren: './order/order.module#OrderModule'}, //LazyModule
-    {path: '**', component: NotFoundComponent} //rota de wildcard página não encontrada
+    {path: 'order', loadChildren: './order/order.module#OrderModule'}, // LazyModule
+    {path: '**', component: NotFoundComponent} // rota de wildcard página não encontrada
 ]
