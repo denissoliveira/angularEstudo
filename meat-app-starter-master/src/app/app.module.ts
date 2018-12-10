4f340f4213b111e251524ea2cac1501182b0ec2c
denissoliveira <denissoliveira@gmail.com>
2018-12-10 19:29:27 -0300

    feat: Disparando o login Manualmente

--- meat-app-starter-master/src/app/app.module.ts
+++ meat-app-starter-master/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { LoginComponent } from './security/login/login.component';
+import { UserDetailComponent } from './header/user-detail/user-detail.component';
 
 
 @NgModule({
@@ -36,7 +37,8 @@ import { LoginComponent } from './security/login/login.component';
     ReviewsComponent,
     OrderSummaryComponent,
     NotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
--- meat-app-starter-master/src/app/security/login/login.service.ts
+++ meat-app-starter-master/src/app/security/login/login.service.ts
@@ -3,15 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { MEAT_API } from 'app/app.api';
 import { User } from './user.model';
-import 'rxjs/add/operator/do'
-import { Router } from '@angular/router';
+import 'rxjs/add/operator/do';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter'
 
 @Injectable()
 export class LoginService {
 
     user: User
+    lastUrl: string
 
-    constructor(private http: HttpClient, private router: Router) {}
+    constructor(private http: HttpClient, private router: Router) {
+        this.router.events.filter(e => e instanceof NavigationEnd)
+                          .subscribe( (e: NavigationEnd) => this.lastUrl = e.url)
+    }
 
     isLoggedIn(): boolean {
         return this.user !== undefined
@@ -22,7 +27,11 @@ export class LoginService {
         .do(user => this.user = user)
     }
 
-    handleLogin(path?: string) {
+    logout() {
+        this.user = undefined
+    }
+
+    handleLogin(path: string = this.lastUrl) { // path: string = this.lastUrl, this.lastUrl é um valou default
         this.router.navigate(['/login', btoa(path)]) // btoa criptografa a url, tem q ser esfeito depois para ser lido
     }
 
